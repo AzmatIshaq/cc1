d6b8f1d0f6e43dc4dab1333fd675e28affb5077b
AzmatIshaq <90857081+AzmatIshaq@users.noreply.github.com>
2021-09-25 16:02:48 -0400

    Added shapes and interactivity with mouse

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -64,6 +64,32 @@ let triangle1 = {
 let angle1 = 0;
 
 
+
+// Variable for rectangle at top
+
+let rectangleTop ={
+
+  x: 0,
+  y: 250,
+  width: 100,
+  lengt: 200,
+  growthRate: 4,
+  speed: 1,
+  fill: 255,
+  alpha: 200,
+  bStroke: 255
+
+};
+
+
+
+
+
+// Random number generator
+
+// let randomNumber = random(10,200);
+
+
 /**
 Description of preload
 */
@@ -96,9 +122,12 @@ function draw() {
     background(bg.red,bg.green,bg.blue);
     bg.red = map(circle1.size,100,width,0,255);
 
-circle1.size =  circle1.size + 0.25;
+
 
 // Circle 1
+    noStroke()
+
+    circle1.size =  circle1.size + 0.25;
 
     circle1.x = circle1.x + circle1.speed; //getting circle1 to move
     circle1.x = constrain(circle1.x,0,width/2); //constrain the circle
@@ -108,6 +137,7 @@ circle1.size =  circle1.size + 0.25;
     ellipse(circle1.x,circle1.y,circle1.size,circle1.size);
 
 // Circle 2
+    noStroke()
     circle2.x = circle2.x + circle2.speed; //getting circle2 to move
     circle2.x = constrain(circle2.x,width/2,width); //constrain the circle
     circle2.size = circle1.size * circle2.sizeRatio;
@@ -116,13 +146,33 @@ circle1.size =  circle1.size + 0.25;
 
 // Triangle 1
 
-
+    noStroke()
     triangle1.y2 = triangle1.y2 + triangle1.growthRate;
     triangle1.y2 = constrain(triangle1.y2, 0, 500)
     triangle(triangle1.x1, triangle1.y1, triangle1.x2, triangle1.y2, triangle1.x3, triangle1.y3);
 
 
 // linke triangle 1 to mouse x and mouse y?
+
+
+// Testing mouseX and mouseY function
+
+  noStroke();
+  rectMode(CENTER);
+  rect(mouseX,mouseY,100,100);
+
+
+
+  // Rectangle at top
+
+
+    stroke(0, 0, rectangleTop.bStroke);
+    strokeWeight(5);
+    fill (0,0,255)
+    rectMode(CENTER);
+    rect(250,50,10,30);
+
+
 
 
 
@@ -133,12 +183,12 @@ circle1.size =  circle1.size + 0.25;
 
 // Testing rotate function
 
+  fill(0,0,0);
+  translate(250, 250);
+  rotate(angle1);
+  stroke(255);
+  line(0,0,50 ,50);
 
- translate(250, 250);
- rotate(angle1);
- stroke(255);
- line(0,0, 50 , 50);
- fill (0,0,0);
 
 
  angle1 = angle1 + 1;
