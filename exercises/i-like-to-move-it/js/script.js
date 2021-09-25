0ea495cec301ad201dcae5fdf6d27a6a228d451e
AzmatIshaq <90857081+AzmatIshaq@users.noreply.github.com>
2021-09-24 23:22:23 -0400

    Update script.js

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -9,10 +9,48 @@ author, and this description to match your project!
 "use strict";
 
 
+let bg = {
+
+  red: 0,
+  green: 0,
+  blue: 0
+
+};
+
+let circle1 = {
+
+  x: 0,
+  y: 250,
+  size: 100,
+  growthRate: 1,
+  speed: 1,
+  fill: 255,
+  alpha: 200
+
+};
+
+let circle2 = {
+
+  x: 500,
+  y: 250,
+  size: 50,
+  sizeRatio: 0.5,
+  speed: -1,
+  fill: 255,
+  alpha: 200
+
+
+
+};
+
+
+
 /**
 Description of preload
 */
 function preload() {
+
+
 }
 
 
@@ -20,6 +58,10 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(500,500);
+  noStroke();
+
+
 
 }
 
@@ -28,5 +70,33 @@ function setup() {
 Description of draw()
 */
 function draw() {
+
+// Background
+
+    background(bg.red,bg.green,bg.blue);
+    bg.red = map(circle1.size,100,width,0,255);
+
+circle1.size =  circle1.size + 0.25;
+
+// Circle 1
+
+    circle1.x = circle1.x + circle1.speed; //getting circle1 to move
+    circle1.x = constrain(circle1.x,0,width/2); //constrain the circle
+    circle1.size =  circle1.size + circle1.growthRate;
+    circle1.size = constrain(circle1.size, 0, width);
+    fill(circle1.fill,circle1.alpha);
+    ellipse(circle1.x,circle1.y,circle1.size,circle1.size);
+
+// Circle 2
+    circle2.x = circle2.x + circle2.speed; //getting circle2 to move
+    circle2.x = constrain(circle2.x,width/2,width); //constrain the circle
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill,circle2.alpha);
+    ellipse(circle2.x,circle2.y,circle2.size,circle2.size);
+
+
+// ellipse(100, 200, 50, 50);
+
+
 
 }
