882849caaf5bfda0db654167ad864e3aebdc2678
AzmatIshaq <90857081+AzmatIshaq@users.noreply.github.com>
2021-10-16 17:11:44 -0400

    P1: Updated comments and added Canvas with Background

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,9 +1,14 @@
 /**
-Title of Project
-Author Name
+Project 1
+Azmat Ishaq
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
+This is a Javascript game made for the CART253 Class at Concordia University.
+This is a game where you can play as Gabe Newell or a Half Life fan. The objective, if you are Gabe, is to avoid the fans from convincing you to make Half Life 3. Just dodge, what they throw at you. Make sure your CONVINCE BAR doesn't fill to the max! If it does, Gabe will have to make Half Life 3 and you will lose the game.
+
+If you are a fan, your objective is to fill the CONVINCE BAR to the max in order to persuade.
+
+Use the arrow keys to move your character. Use the space bar to launch objects if you are a fan.
 */
 
 "use strict";
@@ -21,6 +26,7 @@ function preload() {
 Description of setup
 */
 function setup() {
+    createCanvas(500, 500);
 
 }
 
@@ -29,5 +35,6 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(0);
 
 }
