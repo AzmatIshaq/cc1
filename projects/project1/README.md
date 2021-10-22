2684711d1130fdc4e3fb35e2fb59c9606f5be1fb
AzmatIshaq <90857081+AzmatIshaq@users.noreply.github.com>
2021-10-21 23:47:40 -0400

    P1: Updated README, cleaned up messy code, updated game mechanics

--- projects/project1/README.md
+++ projects/project1/README.md
@@ -3,3 +3,27 @@ CART 253 with professor Pippin Barr
 Project 1
 
 By Azmat Ishaq
+
+# Project 1
+
+CART 253 - With Professor Barr
+by Azmat Ishaq
+
+This is a Javascript game made for the CART253 Class at Concordia University.
+
+This is a game where you can play as Gabe Newell. The objective is to avoid the Half Life 3 projectiles from convincing you to make Half Life 3.
+
+Just dodge, what comes at you. Make sure your CONVINCE BAR doesn't fill to the max!
+
+If it does, Gabe will have to make Half Life 3 and you will lose the game.
+
+Use the arrow keys to move your character.
+
+
+Image sources:
+
+Gabe Newell Half-Life 2: Episode Three Team Fortress 2 Portal PNG, Clipart, Celebrities, Cheek, Digital Distribution, Dota 2, Ear Free PNG Download
+https://imgbin.com/png/LiicBNVr/gabe-newell-half-life-2-episode-three-team-fortress-2-portal-png
+
+Half-Life 2: Episode Three Team Fortress 2 Half-Life: Opposing Force Valve Corporation PNG, Clipart, Area, Art, Brand, Circle, Counterstrike Source Free PNG Download
+https://imgbin.com/png/WmrtdxAW/half-life-2-episode-three-team-fortress-2-half-life-opposing-force-valve-corporation-png
--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -4,11 +4,16 @@ Azmat Ishaq
 
 
 This is a Javascript game made for the CART253 Class at Concordia University.
-This is a game where you can play as Gabe Newell or a Half Life fan. The objective, if you are Gabe, is to avoid the fans from convincing you to make Half Life 3. Just dodge, what they throw at you. Make sure your CONVINCE BAR doesn't fill to the max! If it does, Gabe will have to make Half Life 3 and you will lose the game.
+This is a game where you can play as Gabe Newell. The objective is to avoid the
+Half Life 3 projectiles from convincing you to make Half Life 3.
 
-If you are a fan, your objective is to fill the CONVINCE BAR to the max in order to persuade.
+Just dodge, what comes at you. Make sure your CONVINCE BAR doesn't fill to the
+max!
 
-Use the arrow keys to move your character. Use the space bar to launch objects if you are a fan.
+If it does, Gabe will have to make Half Life 3 and you will lose the game.
+
+Use the arrow keys to move your character.
+
 */
 
 "use strict";
@@ -25,13 +30,16 @@ let circleGaben = {
 let squareFan = {
   x: 250,
   y: 80,
-
+  width: 40,
+  height: 40,
+  fill: 0,
 };
 
 let circleH3 = {
   x: squareFan.x,
   y: squareFan.y,
-  size: 40,
+  fill: 0,
+  size: 80,
   speed: 3,
 };
 
@@ -44,6 +52,8 @@ let health = {
 
 let timer = {
   width: 0,
+  speed: 0.08,
+
 };
 
 // Image variables
@@ -51,10 +61,6 @@ let timer = {
 let gabenImg = undefined;
 let h3IconImg = undefined;
 let gabenWinImg = undefined;
-
-// Sound variables
-
-let barkSFX = undefined;
 
 // Title State
 
@@ -64,10 +70,9 @@ let state = `title`;
 Preload contains images and sound.
 */
 function preload() {
-  h3IconImg = loadImage(`assets/images/half_life_logo_pixelated.png`);
+  h3IconImg = loadImage(`assets/images/2_new_HL3_logo.png`);
   gabenImg = loadImage(`assets/images/gaben_pixelated2.png`);
   gabenWinImg = loadImage(`assets/images/510px-Gabe_Newell_-_2002.jpg`);
-  barkSFX = loadSound(`assets/sounds/bark.wav`);
 }
 
 /**
@@ -75,10 +80,6 @@ Setup is a basic canvas with text formatting
 */
 function setup() {
   createCanvas(500, 500);
-
-  // Formatting for state text
-  textSize(32);
-  textAlign(CENTER, CENTER);
 }
 
 /**
@@ -90,12 +91,10 @@ function draw() {
 
   movement();
 
-  gameTime();
-
   // Title state
 
   if (state === `title`) {
-      title();
+    title();
   }
 
   // Animation State
@@ -111,8 +110,7 @@ function draw() {
     endingWin();
   }
 
-
-  // Ending state
+  // Lose state
 
   if (state === `endingLose`) {
 
@@ -140,15 +138,13 @@ function keyPressed() {
 
 function animation() {
 
-
   // Fan rectangle
 
   push();
-  rectMode(CENTER)
-  rect(squareFan.x, squareFan.y, 40, 40);
+  rectMode(CENTER);
+  fill(squareFan.fill);
+  rect(squareFan.x, squareFan.y, squareFan.width, squareFan.height);
   pop();
-
-
 
   // Gaben's ellipse
 
@@ -157,33 +153,24 @@ function animation() {
   strokeWeight(2);
   ellipseMode(CENTER);
   ellipse(circleGaben.x, circleGaben.y, circleGaben.size);
-  // circleGaben.x = constrain(circleGaben.x, 0, width);
   pop();
 
   // Add Gaben image to ellipse
 
   imageMode(CENTER);
-  image(gabenImg, circleGaben.x, circleGaben.y, 100, 100);
+  image(gabenImg, circleGaben.x, circleGaben.y, width / 5, height / 5);
 
   // Constrain Gaben and easter egg
+  // If your Convince amount is above
 
   if (health.width < 120) {
-      circleGaben.x = constrain(circleGaben.x, circleGaben.size / 2, width - circleGaben.size / 2)
+    circleGaben.x = constrain(circleGaben.x, circleGaben.size / 2, width - circleGaben.size / 2)
+  } else if (health.width > 120) {
+    circleGaben.x = constrain(circleGaben.x, circleGaben.size / 2, width * 1.1)
   }
-
-  else if (health.width > 120) {
-      circleGaben.x = constrain(circleGaben.x, 0, 548)
-  }
-
-
-
-
-
-
-
-
 
   // Half Life 3 projectile ellipse
+
   push();
   fill(0);
   stroke(243, 0, 43);
@@ -195,7 +182,7 @@ function animation() {
   // Add Half Life 3 image to circleH projectile ellipse
 
   imageMode(CENTER);
-  image(h3IconImg, circleH3.x, circleH3.y, 50, 50);
+  image(h3IconImg, circleH3.x, circleH3.y, 80, 80);
 
   // Resetting the h3 projectile
 
@@ -204,79 +191,84 @@ function animation() {
     circleH3.x = squareFan.x;
   }
 
+  // Half life 3 logo starting position
 
-      // Light show to add ambiance
+  imageMode(CENTER);
+  image(h3IconImg, squareFan.x, squareFan.y, 50, 50);
 
-      push();
-      for (let i = 0; i < 3; i++) {
-        let x1 = random(0, width);
-        let y1 = random(0, width);
-        let x2 = random(0, width);
-        let y2 = random(0, width);
-        stroke(0, 255, 0);
-        line(x1, y1, x2, y2);
-      }
-      pop();
+  // Light show
+  push();
+  for (let i = 0; i < 2; i++) {
+    let x1 = random(squareFan.x, squareFan.x);
+    let y1 = random(squareFan.y, squareFan.y);
+    let x2 = random(0, 500);
+    let y2 = random(0, 500);
+    stroke(255, 0, 0);
+    line(x1, y1, x2, y2);
+  }
+  pop();
 
-      // Timer
+  // Timer
 
-      // Timer outline and text
+  // Timer outline and text
 
-      textSize(12);
-      text(`Timer`, 100, 12);
+  push();
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(12);
+  text(`Timer`, 100, 12);
+  pop();
 
-      push();
-      stroke(255);
-      noFill();
-      rectMode(CENTER);
-      rect(100, 28, 140, 20);
-      pop();
+  push();
+  stroke(255);
+  noFill();
+  rectMode(CENTER);
+  rect(100, 28, 140, 20);
+  pop();
 
-    // Timer bar
+  // Timer bar
 
-    push();
-    noStroke();
-    fill(0, 161, 63);
-    rectMode();
-    rect(30.5, 18.5, timer.width, 19);
-    pop();
+  push();
+  noStroke();
+  fill(0, 161, 63);
+  rectMode();
+  rect(30.5, 18.5, timer.width, 19);
+  pop();
 
+  if (timer.width > 139.5) {
+    state = `endingWin`;
+  }
 
-    if (timer.width > 139.5) {
-          state = `endingWin`;
-          }
+  // Convince meter outline and text
 
+  push();
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(12);
+  text('Convince Meter', 400, 12);
+  pop();
 
-    // Convince meter outline and text
+  push();
+  stroke(255);
+  noFill();
+  rectMode(CENTER);
+  rect(400, 28, 140, 20);
+  pop();
 
-    textSize(12);
-    text('Convince Meter', 400, 12);
+  // Convince meter heatlh bar
 
-    push();
-    stroke(255);
-    noFill();
-    rectMode(CENTER);
-    rect(400, 28, 140, 20);
-    pop();
+  push();
+  noStroke();
+  fill(255, 161, 0);
+  rectMode(CENTER);
+  rect(400, 28, health.width, 19);
+  pop();
 
-    // Convince meter heatlh bar
+  // If Convince meter fills, the game ends
 
-    push();
-    noStroke();
-    fill(255, 161, 0);
-    rectMode(CENTER);
-    rect(400, 28, health.width, 19);
-    pop();
-
-    // If Convince meter fills, the game ends
-
-    if (health.width > 139) {
-      state = `endingLose`;
-      }
-
-
-
-
+  if (health.width > 139) {
+    state = `endingLose`;
+  }
 
 }
 
@@ -285,12 +277,10 @@ function movement() {
   // Movement for h3 projectile
 
   circleH3.x = constrain(circleH3.x, 0, width);
-
-//  for (let i = 0; i < 1; i++) {}
   if (state === `animation`) {
     circleH3.y = circleH3.y + circleH3.speed;
     if (random() < 1) {
-    circleH3.x = circleH3.x + random(-10, 10);
+      circleH3.x = circleH3.x + random(-20, 20);
     }
   }
 
@@ -304,7 +294,6 @@ function movement() {
     circleGaben.x = circleGaben.x + circleGaben.speed;
   }
 
-
   // Convince bar increase based on distance to Gaben
 
   let d = dist(circleH3.x, circleH3.y, circleGaben.x, circleGaben.y);
@@ -315,15 +304,19 @@ function movement() {
   // Movement for timer
 
   if (state === `animation`) {
-    timer.width = timer.width + 0.05;
+    timer.width = timer.width + timer.speed;
 
-    }
+  }
 }
 
 function title() {
   // Opening text and instructions
+  push();
   fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
   text(`Half Life 3 vs GABEN`, width / 2, height / 4);
+  pop();
 
   push();
   textSize(16);
@@ -339,35 +332,46 @@ function title() {
 
 function endingWin() {
 
+  push();
   fill(255);
   stroke(255);
   strokeWeight(2);
-
   textSize(16);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
   text(`CONGRATULATIONS YOU WIN!`, width / 2, height / 10);
+  pop();
+
   imageMode(CENTER);
   image(gabenWinImg, width / 2, height / 2, width / 1.5, height / 1.5);
 
+  push();
+  fill(255);
+  noStroke();
+  strokeWeight(2);
+  textSize(16);
+  textAlign(CENTER, CENTER);
+  text(`Press Any Key to Continue`, width / 2, height / 7.5);
+  pop();
 }
 
-function endingLose(){
+function endingLose() {
 
   //Ending
+  push();
   fill(255);
   textSize(16);
-  textAlign(CENTER,CENTER)
+  textAlign(CENTER, CENTER);
   text(`GAME OVER`, width / 2, height / 2);
   text(`Press Any Key to Continue`, width / 2, height / 1.5);
-
+  pop();
 }
 
 function gameReset() {
-  state = `title`
-  health.width = 0
-  timer.width = 0
-  circleH3.x = squareFan.x
-  circleH3.y = squareFan.y
+  state = `title`;
+  health.width = 0;
+  timer.width = 0;
+  circleH3.x = squareFan.x;
+  circleH3.y = squareFan.y;
 
 }
 
