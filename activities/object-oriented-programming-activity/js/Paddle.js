e26afb8c3667f4762596b5779788c26d85e7ce8f
AzmatIshaq <90857081+AzmatIshaq@users.noreply.github.com>
2021-10-30 17:06:45 -0400

    Activity: Completed Object Oriented Programming activity

--- activities/object-oriented-programming-activity/js/Paddle.js
+++ activities/object-oriented-programming-activity/js/Paddle.js
@@ -1,7 +1,24 @@
 // Paddle Class
 
 class Paddle {
-  constructor () {{
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    this.x = 0;
+    this.y = height - this.height / 2;
   }
+
+  move() {
+    this.x = mouseX;
   }
+
+  display() {
+    push();
+    noStroke();
+    fill(140,140,255);
+    rectMode(CENTER);
+    rect(this.x, this.y, this.width, this.height);
+    pop();
+  }
+
 }
--- activities/object-oriented-programming-activity/js/script.js
+++ activities/object-oriented-programming-activity/js/script.js
@@ -7,6 +7,10 @@ Juggling Simulation
 
 "use strict";
 
+let gravityforce = 0.0025;
+let numBalls = 20;
+let paddle;
+let balls = [];
 
 /**
 Description of preload
@@ -19,14 +23,37 @@ function preload() {
 /**
 Description of setup
 */
-function setup() {
-  createCanvas(500,500);
-}
 
+function setup() {
+   createCanvas(windowWidth, windowHeight / 1.2);
+   paddle = new Paddle(200, 20);
+
+   for(let i = 0; i < numBalls; i++) {
+     let x = random(0,width);
+     let y = random(-400, -100);
+     let ball = new Ball(x,y);
+     balls.push(ball)
+   }
+
+
+}
 
 /**
 Description of draw()
 */
 function draw() {
+    background(0);
 
+    paddle.move();
+    paddle.display();
+
+    for (let i = 0; i < balls.length; i++) {
+      let ball = balls[i];
+      if (ball.active){
+      ball.gravity(gravityforce);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
 }
